b55e9e9f91b0b72e868674bc7ae9c4e092517592
Damian Savage <savage.damian@outlook.com>
2025-02-20 22:53:34 -0600

    Added skills and project models

--- content/views.py
+++ content/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
 def project_list_view(request):
-    return render(request, 'content/project_list.html')
+    projects = Project.objects.all()
+    
+    return render(request, 'content/project_list.html', {'projects': projects})
+
 
